f76f1b4232e4e2ddddc95b37f9864f5463f7eaf1
AndOliver46 <anderson_souza75@live.com>
2022-09-27 16:38:33 -0300

    Switching player each turn

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -46,6 +47,13 @@ public class UI {
 		}
 	}
 
+	public static void printMatch(ChessMatch chessMatch) {
+		printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.println("Turn: " + chessMatch.getTurn());
+		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+	}
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print(8 - i + "   ");
@@ -54,10 +62,10 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.print("\n    a b c d e f g h");
+		System.out.print("\n    a b c d e f g h\n");
 	}
-	
-	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {	
+
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print(8 - i + "   ");
 			for (int j = 0; j < pieces.length; j++) {
@@ -69,7 +77,7 @@ public class UI {
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
-		if(background) {
+		if (background) {
 			System.out.print(ANSI_BLUE_BACKGROUND);
 		}
 		if (piece == null) {
